517b25ed74767c7dbb7b0eaf10d31d10884ed948
max Icolumi <max77648@gmail.com>
2021-05-03 21:39:51 +0300

    Добавил очередь

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 
-
 template<typename T>
 struct Stack
 {
 
 	T* Data;
-	size_t Size;
+	size_t Count;
 
 	Stack()
 	{
-		Data = NULL;
-		Size = 0;
+		Data = nullptr;
+		Count = 0;
 	}
 
 	void push(T item)
@@ -20,15 +19,15 @@ struct Stack
 		try
 		{
 			oldData = Data;
-			Data = new T[Size + 1];
-			Size++;
-			for(size_t i = 0; i < Size - 1; i++)
+			Data = new T[Count + 1];
+			Count++;
+			for(size_t i = 0; i < Count - 1; i++)
 			{
 				Data[i] = oldData[i];
 			}				
 
-			Data[Size - 1] = item;
-			if(Size > 1)
+			Data[Count - 1] = item;
+			if(Count > 1)
 			{
 				delete[] oldData;
 			}
@@ -41,19 +40,19 @@ struct Stack
 
 	T pop()
 	{
-		if(Size > 0)
+		if(Count > 0)
 		{
 			try
 			{
 				T* oldData;
 				oldData = Data;
-				Data = new T[Size - 1];
-				Size--;
-				for(size_t i = 0; i < Size; i++)
+				Data = new T[Count - 1];
+				Count--;
+				for(size_t i = 0; i < Count; i++)
 				{
 					Data[i] = oldData[i];
 				}
-				T data = oldData[Size];
+				T data = oldData[Count];
 				delete[] oldData;
 				return data;
 
@@ -72,15 +71,97 @@ struct Stack
 
 	bool is_empty()
 	{
-		return Size == 0;
+		return Count == 0;
 	}
 
 	~Stack()
 	{
-		if(Size > 0)
+		if(Count > 0)
 		{
 			delete[] Data;
 		}
+	}
+};
+
+template<typename T>
+struct Queue
+{
+	T* Data;
+	size_t Count;
+	
+	bool is_empty()
+	{
+		return Count == 0;
+	}
+
+	Queue()
+	{
+		Data = nullptr;
+		Count = 0;
+	}
+
+	void push(T item)
+	{
+		T* oldData;
+		oldData = Data;
+
+		try
+		{
+			Data = new T[Count + 1];
+					
+			for(size_t i = 0; i < Count; i++)
+			{
+				Data[i] = oldData[i];
+			}
+
+			Data[Count] = item;
+			Count++;
+			if(Count > 1)
+				delete[] oldData;
+		}
+		catch(std::bad_alloc e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+
+	T pop()
+	{
+		if(Count > 0)
+		{
+			T item;
+			item = Data[0];
+			try
+			{
+				T* p2;
+				p2 = new T[Count - 1];
+				Count--;
+				for(size_t i = 0; i < Count; i++)
+				{
+					p2[i] = Data[i + 1];
+				}				
+
+				  
+				if(Count > 0)
+				{
+					delete[] Data;
+				}
+					
+				Data = p2;
+
+				return item;
+			}
+			catch(std::bad_alloc e)
+			{
+				std::cout << e.what() << std::endl;
+				return 0;
+			}
+		}
+		else
+		{
+			std::cout << "Queue is empty\n";
+			exit(0);
+		}		
 	}
 };
 
@@ -96,7 +177,16 @@ int main(int argc, char** argv)
 	cout << a.pop() << endl;
 	cout << a.pop() << endl;
 	cout << a.pop() << endl;
-	cout << a.pop() << endl;
+
+	Queue<int> b;
+	b.push(1);
+	b.push(2);
+	b.push(3);
+
+	cout << b.pop() << endl;
+	cout << b.pop() << endl;
+	cout << b.pop() << endl;
+	cout << b.pop() << endl;
 
 	system("pause");
 	return 0;
